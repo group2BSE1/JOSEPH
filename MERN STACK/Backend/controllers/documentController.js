797580e0a3cc12cafa2d3cea96c8a255fefbf02e
unknown <Admin@UAT.trobank.com>
2023-09-08 09:32:10 +0300

    Second commit

---
+++ MERN STACK/Backend/controllers/documentController.js
@@ -0,0 +1,58 @@
+const Document = require("../models/documentModel");
+const mongoose = require("mongoose");
+
+// get all documents
+const getDocuments = async (req, res) => {
+  const documents = await Document.find({}).sort({ createdAt: -1 });
+  res.status(200).json(documents);
+};
+
+//get a single document
+const getDocument = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such document" });
+  }
+  const document = await Document.findById(id);
+
+  if (!document) {
+    return res.status(404).json({ error: "No such workout" });
+  }
+
+  res.status(200).json(document);
+};
+
+// create a new document
+const createDocument = async (req, res) => {
+  const { title, description, author, size } = req.body;
+
+  // add doc to db
+  try {
+    const document = await Document.create({
+      title,
+      description,
+      author,
+      size,
+    });
+    res.status(200).json(document);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// delete a document
+const deleteDocument = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such document" });
+  }
+};
+
+//update a document
+
+module.exports = {
+  getDocuments,
+  getDocument,
+  createDocument,
+};
--- MERN STACK/Backend/routes/documents.js
+++ MERN STACK/Backend/routes/documents.js
@@ -1,34 +1,20 @@
 const express = require("express");
-const Document = require("../models/documentModel");
+const {
+  createDocument,
+  getDocument,
+  getDocuments,
+} = require("../controllers/documentController");
 
 const router = express.Router();
 
 // GET all documents
-router.get("/", (req, res) => {
-  res.json({ msg: "GET all Documents" });
-});
+router.get("/", getDocuments);
 
 // GET a single document
-router.get("/:id", (req, res) => {
-  res.json({ msg: "GET a single Documents" });
-});
+router.get("/:id", getDocument);
 
 // POST a new document
-router.post("/", async (req, res) => {
-  const { title, description, author, size } = req.body;
-
-  try {
-    const document = await Document.create({
-      title,
-      description,
-      author,
-      size,
-    });
-    res.status(200).json(document);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post("/", createDocument);
 
 // DELETE a document
 router.delete("/:id", (req, res) => {
