293f4d6f5c972c57f37e39a0ddd28a4d2a31bdd4
ZEUS <mwesigyejosephug0@gmail.com>
2023-09-11 21:30:48 +0300

    authentication context added

--- MERN STACK/frontend/src/index.js
+++ MERN STACK/frontend/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { DocumentsContextProvider } from "./context/DocumentContext";
+import { AuthContextProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <DocumentsContextProvider>
-      <App />
-    </DocumentsContextProvider>
+    <AuthContextProvider>
+      <DocumentsContextProvider>
+        <App />
+      </DocumentsContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
