3d3ef3fc7aec7e99543afb16a14df04190ad5715
ZEUS <mwesigyejosephug0@gmail.com>
2023-09-11 21:11:03 +0300

    web tokens added

--- MERN STACK/Backend/models/userModel.js
+++ MERN STACK/Backend/models/userModel.js
@@ -1,11 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const validator = require("validator");
-const jwt = require("jsonwebtoken");
-
-const createToken = (_id) => {
-  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
-};
 
 const Schema = mongoose.mongoose.Schema;
 
@@ -46,6 +41,27 @@ userSchema.statics.signup = async function (email, password) {
   const hash = await bcrypt.hash(password, salt);
 
   const user = await this.create({ email, password: hash });
+
+  return user;
+};
+
+//static login method
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw Error("Incorrect Email");
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw Error("Incorrect password");
+  }
 
   return user;
 };
